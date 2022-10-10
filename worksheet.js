b50ee4f2690a98468b97da236e7c9e2f97f4369b
WallochMatt <matthewrwalloch@gmail.com>
2022-10-10 13:21:10 -0500

    feat: Completed For Loops and Functions sections

--- worksheet.js
+++ worksheet.js
@@ -35,17 +35,77 @@
 
         // Conditionals Task 2: Random Number
 
-let randomNumber = Math.floor(Math.random() * 11);
+// let randomNumber = Math.floor(Math.random() * 11);
 
-if(randomNumber >= 0 && randomNumber <= 2){
-    console.log("Beatles")
-}
-else if(randomNumber >= 3 && randomNumber <= 5){
-    console.log("Stones")
-}
-else if(randomNumber >= 6 && randomNumber <= 8){
-    console.log("Floyd")
-}
-else if(randomNumber >= 9 && randomNumber <= 10){
-    console.log("Hendrix")
-}
+// if(randomNumber >= 0 && randomNumber <= 2){
+//     console.log("Beatles")
+// }
+// else if(randomNumber >= 3 && randomNumber <= 5){
+//     console.log("Stones")
+// }
+// else if(randomNumber >= 6 && randomNumber <= 8){
+//     console.log("Floyd")
+// }
+// else if(randomNumber >= 9 && randomNumber <= 10){
+//     console.log("Hendrix")
+// }
+
+
+        // "For" Loops Task 1
+
+// for(let i=0; i<7; i++){
+//     console.log("JavaScript is cool!")
+// }
+
+        // "For" Loops Task 2
+
+// var numbersToTen = 0
+
+// for(let i = 0; i<= 10; i++){
+//     console.log(numbersToTen)
+//     numbersToTen ++
+// }
+
+// for(let newNumToTen = 0; newNumToTen <= 10; newNumToTen++){
+//     console.log(newNumToTen)
+// } // Better Version ^
+
+        // "For" Loops Task 3
+
+// for(let i = 0; i<= 9; i++){
+//     if(i % 2 === 0){
+//         console.log("hello")
+//     }
+//     else(console.log("goodbye"))
+// }
+
+    
+        //Functions Task 1: Favorite Movie (void functions)
+
+// function printMovieName(){
+//     let favMovie = "Titan A.E."
+//     console.log(favMovie)
+// }
+
+// printMovieName()
+
+        //Function Task 2: Favorite Band (return function)
+
+// function userGiveFavBand(){
+//     input = prompt("What is your favorite band?")
+//     return input
+// }
+
+// let usersFavBand = userGiveFavBand()
+// console.log(usersFavBand)
+
+        // Function Task 3: Concert (parameters)
+
+// function concertDisplay(musicalAct){
+//     myStreet = prompt("What street do you live on?");
+//     console.log(`It would be great if ${musicalAct} played a show on ${myStreet}!`)
+// }
+// concertDisplay("Green Day")
+
+
+        // Arrays Task 1: Desktop Items
